7f2fe83a544bed9c4e291b57bd9e2fee43f9a5a6
boxme <desmond.ng.yang.yi@gmail.com>
2013-12-05 16:27:37 +0800

    Robot. Rmb to consider all the components that make up a state in graph problem

--- UVa/UVa 314/Main.java
+++ UVa/UVa 314/Main.java
@@ -1,190 +1,109 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-class IntegerPair {
-	public int row;
-	public int col;
-	public int turn;
-	public int steps;
-	public String direction;
-	
-	public IntegerPair(int row, int col) {
-		this.row = row;
-		this.col = col;
-		this.turn = 0;
-		this.steps = 0;
-	}
-}
-
-class UVa314 {
-	private int R;
-	private int C;
-	private IntegerPair[][] map;
-	private boolean[][] bomb;
-	private IntegerPair start;
-	
-	public UVa314(int R, int C) {
-		this.R = R;
-		this.C = C;
-		map = new IntegerPair[R+1][C+1];
-		bomb = new boolean[R+1][C+1];
-	}
-	
-	public void insert_obstacles(int row, int col) {
-		bomb[row+1][col+1] = true;
-		bomb[row+1][col] = true;
-		bomb[row][col] = true;
-		bomb[row][col+1] = true;
-	}
-	
-	public void BFS(int r, int c, String direction) {
-		Queue<IntegerPair> q = new LinkedList<IntegerPair>();
-		IntegerPair start = new IntegerPair(r, c);
-		start.direction = direction;
-		this.start = start;
-		map[r][c] = start;
-		q.offer(start);
-		
-		while (!q.isEmpty()) {
-			IntegerPair parent = q.poll();
-			
-			if (parent.row < R-1 && parent.col != 0 && parent.col != C && !bomb[parent.row+1][parent.col]) {
-				IntegerPair new_road = new IntegerPair(parent.row+1, parent.col);
-				new_road.direction = "south";
-				new_road.steps = change_direction(parent, new_road.direction);
-				new_road.turn = update_turn(parent, new_road);
-				if (map[parent.row+1][parent.col] != null && new_road.steps < map[parent.row+1][parent.col].steps) {
-					q.offer(new_road);
-					map[parent.row+1][parent.col] = new_road;
-				}
-				else if (map[parent.row+1][parent.col] == null) {
-					q.offer(new_road);
-					map[parent.row+1][parent.col] = new_road;
-				}
-			}
-			if (parent.row > 1 && parent.col != 0 && parent.col != C && !bomb[parent.row-1][parent.col]) {
-				IntegerPair new_road = new IntegerPair(parent.row-1, parent.col);
-				new_road.direction = "north";
-				new_road.steps = change_direction(parent, new_road.direction);
-				new_road.turn = update_turn(parent, new_road);
-				if (map[parent.row-1][parent.col] != null && new_road.steps < map[parent.row-1][parent.col].steps) {
-					q.offer(new_road);
-					map[parent.row-1][parent.col] = new_road;
-				}
-				else if (map[parent.row-1][parent.col] == null) {
-					q.offer(new_road);
-					map[parent.row-1][parent.col] = new_road;
-				}
-			}
-			if (parent.col < C-1 && parent.row != 0 && parent.row != R && !bomb[parent.row][parent.col+1]) {
-				IntegerPair new_road = new IntegerPair(parent.row, parent.col+1);
-				new_road.direction = "east";
-				new_road.steps = change_direction(parent, new_road.direction);
-				new_road.turn = update_turn(parent, new_road);
-				if (map[parent.row][parent.col+1] != null && new_road.steps < map[parent.row][parent.col+1].steps) {
-					q.offer(new_road);
-					map[parent.row][parent.col+1] = new_road;
-				}
-				else if (map[parent.row][parent.col+1] == null) {
-					q.offer(new_road);
-					map[parent.row][parent.col+1] = new_road;
-				}
-			}
-			if (parent.col > 1 && parent.row != 0 && parent.row != R && !bomb[parent.row][parent.col-1]) {
-				IntegerPair new_road = new IntegerPair(parent.row, parent.col-1);
-				new_road.direction = "west";
-				new_road.steps = change_direction(parent, new_road.direction);
-				new_road.turn = update_turn(parent, new_road);
-				if (map[parent.row][parent.col-1] != null && new_road.steps < map[parent.row][parent.col-1].steps) {
-					q.offer(new_road);
-					map[parent.row][parent.col-1] = new_road;
-				}	
-				else if (map[parent.row][parent.col-1] == null) {
-					q.offer(new_road);
-					map[parent.row][parent.col-1] = new_road;
-				}
-			}
-		}
-	}
-	
-	private int change_direction(IntegerPair parent, String direction) {
-		if (direction.equalsIgnoreCase(parent.direction)) return check_state(parent);
-		else if (parent.direction.equals("north") || parent.direction.equals("south")) {
-			if (direction.equals("east") || direction.equals("west"))   return parent.steps+2;
-			else											            return parent.steps+3;
-		}
-		else if (parent.direction.equals("east") || parent.direction.equals("west")) {
-			if (direction.equals("north")|| direction.equals("south"))  return parent.steps+2;
-			else											  			return parent.steps+3;
-		}
-		return 0;
-	}
-	
-	private int check_state(IntegerPair parent) {
-		if (parent.turn == 0) return parent.steps+1;
-		else return parent.steps;
-	}
-	
-	private int update_turn(IntegerPair parent, IntegerPair current) {
-		if (!parent.direction.equalsIgnoreCase(current.direction)) return 1;
-		if (parent.turn < 2) return parent.turn+1;
-		else				 return 0;
-	}
-	
-	public int get_min_steps(int r, int c) {
-		if (start.row == r && start.col == c) return 0;
-		else if (map[r][c] == null || bomb[r][c] || r == 0 || c == 0) return -1;
-		else				   return map[r][c].steps;
-	}
-}
+class UVa314{}
 
 class Main {
-	
-	void run() throws Exception{
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-	    
-	    StringTokenizer st = new StringTokenizer(br.readLine());
-	    int row = Integer.parseInt(st.nextToken());
-	    int col = Integer.parseInt(st.nextToken());
-	    
-	    while (row != 0 && col != 0) {
-	    	UVa314 compute = new UVa314(row, col);
-	    	
-	    	for (int i = 0; i < row; i++) {
-		    	st = new StringTokenizer(br.readLine());
-		    	for (int j = 0; j < col; j++) {
-		    		int c = Integer.parseInt(st.nextToken());
-		    		if (c == 1) 
-			    		compute.insert_obstacles(i, j);
-		    	}
-			}
-	    	
-	    	st = new StringTokenizer(br.readLine());
-	    	int start_r = Integer.parseInt(st.nextToken());
-	    	int start_c = Integer.parseInt(st.nextToken());
-	    	int end_r = Integer.parseInt(st.nextToken());
-	    	int end_c = Integer.parseInt(st.nextToken());
-	    	String direction = st.nextToken();
-	    	compute.BFS(start_r, start_c, direction);
-	    	pr.println(compute.get_min_steps(end_r, end_c));
-	    	
-	    	
-	    	st = new StringTokenizer(br.readLine());
-		    row = Integer.parseInt(st.nextToken());
-		    col = Integer.parseInt(st.nextToken());
-	    }
-	    pr.close();
-	}
-	
-	public static void main(String[] args) throws Exception {
-		Main program = new Main();
-		program.run();		
-	}
+    private final int[] dx = {-1, 0, 1, 0};    //NESW Go left/right
+    private final int[] dy = {0, 1, 0, -1};    //Go top/bottom
+    private int[][] store;
+    private int[][][] taken;
+    private int b1, b2, e1, e2, d, N, M;
+
+    void bfs() {
+        if (b1 == e1 && b2 == e2) {
+            System.out.println(0); return;
+        }
+        taken = new int[105][105][4];
+        taken[b1][b2][d] = 1;
+        Queue<Integer> qx = new LinkedList<Integer>();
+        Queue<Integer> qy = new LinkedList<Integer>();
+        Queue<Integer> qd = new LinkedList<Integer>();
+        qx.offer(b1); qy.offer(b2); qd.offer(d);
+        while (!qx.isEmpty()) {
+            int x = qx.poll(); int y = qy.poll(); int dir = qd.poll();
+            int turn_x = x + dx[dir]; int turn_y = y + dy[dir];
+
+            for (int i = 1; i <= 3; ++i) {
+                if (turn_x < 1 || turn_y < 1 || turn_x >= N || turn_y >= M) break;
+
+                if (store[turn_x][turn_y] == 1) continue;
+                if (taken[turn_x][turn_y][dir] == 0) {
+                    taken[turn_x][turn_y][dir] = taken[x][y][dir] + 1;
+                    qx.offer(turn_x); qy.offer(turn_y); qd.offer(dir);
+                }
+                turn_x += dx[dir]; turn_y += dy[dir];
+            }
+
+            turn_x = x; turn_y = y;
+            int turn_dir = (dir+1)%4;    //Rotate right
+            if (taken[turn_x][turn_y][turn_dir] == 0) {
+                taken[turn_x][turn_y][turn_dir] = taken[x][y][dir] + 1;
+                qx.offer(turn_x); qy.offer(turn_y); qd.offer(turn_dir);
+            }
+
+            turn_dir = (dir+3)%4;        //Rotate left
+            if (taken[turn_x][turn_y][turn_dir] == 0) {
+                taken[turn_x][turn_y][turn_dir] = taken[x][y][dir] + 1;
+                qx.offer(turn_x); qy.offer(turn_y); qd.offer(turn_dir);
+            }
+
+            if (taken[e1][e2][0] > 0 || taken[e1][e2][1] > 0 || taken[e1][e2][2] > 0 || taken[e1][e2][3] > 0) 
+                break;
+        }
+
+        int answer = 0;
+        for (int i = 0; i < 4; ++i) {
+            if (taken[e1][e2][i] > 0)
+                answer = taken[e1][e2][i];
+        }
+        System.out.println(answer-1);
+    }
+
+    void run() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt(); M = scanner.nextInt();
+        while (N != 0 && M != 0) {
+            store = new int[N+1][M+1];
+            for (int i = 1; i <= N; ++i) {
+                for (int j = 1; j <= M; ++j) {
+                    int k = scanner.nextInt();
+                    if (k == 1) {
+                        store[i-1][j-1] = store[i-1][j] = 1;
+                        store[i][j-1] = store[i][j] = 1;
+                    }
+                }
+            }
+
+            b1 = scanner.nextInt(); b2 = scanner.nextInt(); e1 = scanner.nextInt(); e2 = scanner.nextInt(); 
+            String dir = scanner.next();
+            if (dir.equals("north")) d = 0;
+            if (dir.equals("east"))  d = 1;
+            if (dir.equals("south")) d = 2;
+            if (dir.equals("west"))  d = 3;
+
+            bfs();
+            N = scanner.nextInt(); M = scanner.nextInt();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        Main program = new Main();
+        program.run();		
+    }
 }
+
+class Edge implements Comparable<Edge> {
+    Integer u, v, weight;
+
+    public Edge(int u, int v, int weight) {
+        this.u = u; this.v = v; this.weight = weight;
+    }
+
+    @Override 
+    public int compareTo(Edge other) {
+        return this.weight - other.weight;
+    }
+}
+

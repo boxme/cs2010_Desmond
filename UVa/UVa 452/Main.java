dd26b73a284f74e6846d02700b78ba298cf00bd5
boxme <desmond.ng.yang.yi@gmail.com>
2013-12-06 15:35:26 +0800

    Project Scheduling. Rmb to use DP when the graph is guaranteed a DAG'

--- UVa/UVa 452/Main.java
+++ UVa/UVa 452/Main.java
@@ -1,107 +1,79 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
-class UVa00452 {}
+class UVa452{}
 
 class Main {
-	private Stack<IntegerPair> stack;
-	private HashMap<String, Boolean> hashtable;
-	private ArrayList<ArrayList<IntegerPair>> adj_list;
-	private ArrayList<Integer> answers = new ArrayList<Integer>();
-	
-	void topo(IntegerPair start) {
-		hashtable.put(start.task, true);
-		
-		for (IntegerPair next : adj_list.get(start.vertice)) {
-			if (hashtable.get(next.task) == null) 
-				topo(next);
-		}
-		stack.add(start);
-	}
-	
-	void dijkstra() {
-		int V = stack.size();
-		int[] longest_path = new int[V];
-		IntegerPair src = stack.peek();
-		longest_path[src.vertice] = src.days;
-		
-		while (!stack.isEmpty()) {
-			IntegerPair current = stack.pop();
-			
-			for (IntegerPair next : adj_list.get(current.vertice)) {
-				if (longest_path[next.vertice] > longest_path[current.vertice] + next.days) 
-					longest_path[next.vertice] = longest_path[current.vertice] + next.days;
-			}
-		}
-		answers.add(-longest_path[V-1]);
-	}
-	
-	void run() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    StringTokenizer st = new StringTokenizer(br.readLine());
-	    
-	    int num_test_cases = Integer.parseInt(st.nextToken());
-    	st = new StringTokenizer(br.readLine());
+    private ArrayList<ArrayList<Integer>> adj_list;
+    private int[] weights, memo, starts;
+    
+    //Use DP if you know the graph is a DAG
+    int dp(int u) {
+    	if (adj_list.get(u).isEmpty()) return weights[u];
+    	if (memo[u] != 0) return memo[u];
+    	
+    	for (Integer next : adj_list.get(u)) 
+			memo[u] = Math.max(memo[u], dp(next) + weights[u]);
+    	
+    	return memo[u];
+    }
 
-	    while (num_test_cases-- > 0) {
-	    	stack = new Stack<IntegerPair>();
-	    	hashtable = new HashMap<String, Boolean>();
-	    	adj_list = new ArrayList<ArrayList<IntegerPair>>();
-	    	
-	    	for (int i = 0; i < 30; i++) 
-				adj_list.add(new ArrayList<IntegerPair>());
-			
-	    	st = new StringTokenizer(br.readLine());
-	    	
-	    	String task = st.nextToken();
-	    	IntegerPair start = new IntegerPair(task, Integer.parseInt(st.nextToken()));
-	    	
-    		st = new StringTokenizer(br.readLine());
-    		
-	    	while (st.hasMoreTokens()) {
-	    		task = st.nextToken();
-	    		IntegerPair new_task = new IntegerPair(task, Integer.parseInt(st.nextToken()));
-	    		String parents = st.nextToken();
-	    		
-	    		for (int i = 0; i < parents.length(); i++) 
-					adj_list.get((parents.charAt(i)-65)).add(new_task);
-	    		
-	    		st = new StringTokenizer(br.readLine());
-			}
-	    	
-	    	topo(start);
-	    	dijkstra();	    	
-	    }
-	    for (int i=0; i< answers.size()-1; ++i) {
-			System.out.println(answers.get(i));
-			System.out.println();
-		}
-	    System.out.println(answers.get(answers.size()-1));
-	}
-	
-	public static void main(String[] args) throws Exception{
-		Main program = new Main();
-		program.run();		
-	}
+    void run() throws Exception {
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
+        int TC = Integer.parseInt(scanner.readLine());
+        scanner.readLine();
+        
+        while (TC-- > 0) {
+            String input = "";
+            weights = new int[27]; starts = new int[27];
+            adj_list = new ArrayList<ArrayList<Integer>>();
+            
+            for (int i = 0; i < 27; i++) 
+            	adj_list.add(new ArrayList<Integer>());
+
+            while (scanner.ready() && !(input = scanner.readLine()).equals("")) {
+                if (input.compareTo("") == 0) break;
+                String[] input_array = input.split(" ");
+                int index = input_array[0].charAt(0) - 65; int weight = Integer.parseInt(input_array[1]);
+                weights[index] = weight; 
+                
+                if (input_array.length == 2) { 
+                	starts[index] = 1;
+                	continue;              	
+                }
+                
+                for (int i = 0; i < input_array[2].length(); ++i) 
+                    adj_list.get(input_array[2].charAt(i)-65).add(index);
+            }
+            
+            memo = new int[27];
+            int ans = 0;
+            for (int i = 0; i < 27; i++)
+            	if (starts[i] == 1) 
+                	ans = Math.max(ans, dp(i));
+            
+            System.out.println(ans);
+            if (TC > 0) System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        Main program = new Main();
+        program.run();		
+    }
 }
 
-class IntegerPair implements Comparable<IntegerPair> {
-	int vertice;
-	String task;
-	int days;
-	
-	public IntegerPair(String task, int days) {
-		this.task = task;
-		this.days = 0-days;
-		this.vertice = task.charAt(0)-65;
-	}
-	
-	@Override
-	public int compareTo(IntegerPair other) {
-		return this.days - other.days;
-	}
+class Edge implements Comparable<Edge> {
+    Integer u, v, weight;
+
+    public Edge(int u, int v, int weight) {
+        this.u = u; this.v = v; this.weight = weight;
+    }
+
+    @Override 
+    public int compareTo(Edge other) {
+        return other.weight - this.weight;
+    }
 }
+
